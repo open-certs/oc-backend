c242be4464df97747ce9d5f06dec5422293c4f47
Shubham-Kumar-2000 <shukhu10@gmail.com>
2022-02-12 23:57:10 +0530

    Feature token redirect

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,10 +6,13 @@ exports.login = (req, res) => {
         delete user._raw;
         const token = sign(user);
         delete user.accessToken;
-        return res.status(200).json({
-            user,
-            token
-        });
+        return res.redirect(
+            `${process.env.FRONTEND_URL}/recievedToken.html?token=${token}`
+        );
+        // return res.status(200).json({
+        //     user,
+        //     token
+        // });
     } catch (e) {
         console.log(e);
         res.status(200).json({
