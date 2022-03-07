02955d7d4f4e64c3eb9a2f7fef614b8102511f4a
Satabrata Paul <satabrata.slg88@gmail.com>
2022-03-07 20:10:04 +0530

    Negative test for getCertificate details by ID

--- test/certificate.test.js
+++ test/certificate.test.js
@@ -1,5 +1,6 @@
 require('./enviroment.test');
-
+require('./database.test');
+const { Types } = require('mongoose');
 const certificateController = require('../src/controllers/certificate.controller');
 const Certificate = require('../src/models/certificate.model');
 
@@ -18,6 +19,25 @@ test('should return certificate details when valid certificate id is provided', 
             expect(String(x.certificate._id)).toBe(
                 String(existingCertificate._id)
             );
+        })
+    };
+
+    await certificateController.getCertDetails(mReq, mRes);
+});
+
+test('should return no certificate details when non-existing certificate id is provided', async () => {
+    const mReq = {
+        params: {
+            id: new Types.ObjectId()
+        }
+    };
+
+    const mRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn((x) => {
+            expect(x).toBeTruthy();
+            expect(x.error).toBeUndefined();
+            expect(x.certificate).toBe(null);
         })
     };
 
---
+++ test/database.test.js
@@ -0,0 +1,11 @@
+require('./enviroment.test');
+const mongoose = require('mongoose');
+beforeAll(() => require('../src/models/index.model').connect());
+
+afterAll(() => {
+    mongoose.connection.close();
+});
+
+test('should connect to mongoDB when loaded', () => {
+    expect(mongoose.connection.readyState).toBe(1);
+});
